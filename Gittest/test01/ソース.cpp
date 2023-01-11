9291d0fb9013ec826dadbdb910b0dc6c6d2dbe78
miyaguni-rikuto <120348141+miyaguni-rikuto@users.noreply.github.com>
2023-01-11 09:56:00 +0900

    Revert "「感謝します」感謝忘れずに"
    This reverts commit 4f42fa4e940958ec065b03d361e0baa133f3ccc5.

--- Gittest/test01/ソース.cpp
+++ Gittest/test01/ソース.cpp
@@ -8,11 +8,6 @@ int main(void)
 	printf("GitHun Hello\n");
 
 	printf("IM XQQ\n");
-	printf("kansyasimasu\n");
-	printf("kansyasimasu\n");
-	printf("kansyasimasu\n");
-	printf("kansyasimasu\n");
-	printf("kansyasimasu\n");
 
 	for (int i = 0; i < 5; i++)
 	{
