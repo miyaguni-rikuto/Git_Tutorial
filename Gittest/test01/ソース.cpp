4f42fa4e940958ec065b03d361e0baa133f3ccc5
miyaguni-rikuto <120348141+miyaguni-rikuto@users.noreply.github.com>
2023-01-11 09:36:47 +0900

    「感謝します」感謝忘れずに

--- Gittest/test01/ソース.cpp
+++ Gittest/test01/ソース.cpp
@@ -5,6 +5,11 @@ int main(void)
 	printf("GitHun Hello\n");
 
 	printf("IM XQQ\n");
+	printf("kansyasimasu\n");
+	printf("kansyasimasu\n");
+	printf("kansyasimasu\n");
+	printf("kansyasimasu\n");
+	printf("kansyasimasu\n");
 
 		return 0;
 }
